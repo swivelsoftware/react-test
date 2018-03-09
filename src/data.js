8158dd071d9a9b724a6bef54a3865cf2afa7ec84
Kennys Ng <kennysng@swivelsoftware.com>
2018-03-09 09:26:03 +0800

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,7 +1,10 @@
 export const headers = []
 
 for (let i = 0, length = 10; i < length; i += 1) {
-  headers.push(`Header ${i}`)
+  headers.push({
+    key: `key${i}`,
+    value: `Header ${i + 1}`
+  })
 }
 
 export const rows = []
@@ -9,7 +12,7 @@ export const rows = []
 for (let i = 0, length = 2000; i < length; i += 1) {
   const item = {}
   for (let j = 0, length2 = headers.length; j < length2; j += 1) {
-    item[headers[j]] = `Item ${i * headers.length + j}`
+    item[headers[j].key] = `Item ${i * headers.length + j + 1}`
   }
-  result.push(item)
+  rows.push(item)
 }
